72b3de4f82c163361712e74ece5f57df39cc0c33
p0x38 <ryuuking0410@gmail.com>
2026-09-03 02:56:36 +0900

    test(chatbot): cover Markov runtime cache reload

---
+++ tests/markov/test_chatbot_runtime.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from poxbot.platforms.discord.extensions.chatbot import ChatbotCog
+from poxbot.platforms.discord.extensions.chatbot_runtime import _clear_markov_runtime
+
+
+def test_clear_markov_runtime_clears_only_runtime_caches() -> None:
+    cog = object.__new__(ChatbotCog)
+    cog.markov_models = {'model': object()}
+    cog.markov_dialogues = {'dialogue': object()}
+
+    cleared = _clear_markov_runtime(cog)
+
+    assert cleared == 2
+    assert cog.markov_models == {}
+    assert cog.markov_dialogues == {}
